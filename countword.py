16c10d51b7085d20b204a6584a22631ae2bdaf74
jasmine-zhu88 <jasminezhu88@gmail.com>
2024-11-17 14:09:48 +0800

    commit

--- countword.py
+++ countword.py
@@ -9,6 +9,6 @@ if __name__=="__main__":
         for (word,count) in output:
             print(f"{word}:{count}")
     def saveFile():
-        sc.textFile("/home/local/Downloads/pg20417.txt").flatMap(lambda line:line.strip().split()).map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b).saveAsTextFile("/home/local/Downloads/wc.txt")
+        sc.textFile("/home/local/Downloads/pg20417.txt").flatMap(lambda line:line.strip().split()).map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b).saveAsTextFile("/home/local/Downloads/wc")
         saveFile()
         sc.stop()

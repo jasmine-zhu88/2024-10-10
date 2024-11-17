a66611d176038e24ac04ca9e2fa4be8a9578198e
jasmine-zhu88 <jasminezhu88@gmail.com>
2024-11-17 14:16:01 +0800

    update countword

--- countword.py
+++ countword.py
@@ -2,13 +2,5 @@ from pyspark import SparkConf,SparkContext
 if __name__=="__main__":
     conf=SparkConf()
     sc=SparkContext(conf=conf)
-
-    def printResult():
-        counts=sc.textFile("/home/local/Downloads/pg20417.txt").flatMap(lambda line:line.strip().split()).map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b)
-        output=counts.collect()
-        for (word,count) in output:
-            print(f"{word}:{count}")
-    def saveFile():
-        sc.textFile("/home/local/Downloads/pg20417.txt").flatMap(lambda line:line.strip().split()).map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b).saveAsTextFile("/home/local/Downloads/wc")
-        saveFile()
-        sc.stop()
+    sc.textFile("/home/local/Downloads/pg20417.txt").flatMap(lambda line:line.strip().split()).map(lambda x:(x,1)).reduceByKey(lambda a,b:a+b).saveAsTextFile("/home/local/Downloads/wc")
+    sc.stop()
